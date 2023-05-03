d3e9bbbd2603ba3dcbeaf41add4a0811e4982fe1
MarsAcai <130596573+MarsAcai@users.noreply.github.com>
2023-05-02 18:39:31 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// toggle menu more
-const menuMore = document.querySelector('.menu-more');
-const menuItemMore = document.querySelector('.menu-item-more');
-
-menuItemMore.addEventListener('click', function() {
-  menuMore.style.display = (menuMore.style.display === 'block') ? 'none' : 'block';
-});
